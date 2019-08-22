6766a9f61e096ea5a3b53908635e52317a7a9268
Peter Dillinger <peterd@fb.com>
2019-08-22 10:06:17 -0700

    Script for digesting some test results

---
+++ bloom_simulation_tests/process_results.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+FILE="$1"
+
+[ -e "$FILE" ] || exit 1
+
+LST="$(cat "$FILE" | cut -d' ' -f1 | sort | uniq)"
+
+(for KEY in $LST; do LINES=`grep -c $KEY "$FILE"`; LINE=$(($LINES / 3 + 1)); echo "$LINE/$LINES" >&2; grep $KEY "$FILE" | sort -n | head -n $LINE | tail -1; done) | sort
